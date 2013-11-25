b34c65f6a94143d107e6c8ebc07a78562b583896
张钦 <zhangqin@xiaomi.com>
2013-11-25 22:47:19 +0800

    project and issues

---
+++ protected/views/issue/_view.php
@@ -0,0 +1,35 @@
+<?php
+/* @var $this IssueController */
+/* @var $data Issue */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), Yii::app()->homeUrl.'issue/'.$data->id); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('name')); ?>:</b>
+	<?php echo CHtml::encode($data->name); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('description')); ?>:</b>
+	<?php echo CHtml::encode($data->description); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('type_id')); ?>:</b>
+	<?php echo CHtml::encode($data->getTypeText()); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('status_id')); ?>:</b>
+	<?php echo CHtml::encode($data->getStatusText()); ?>
+	<br />
+	<b><?php echo CHtml::encode($data->getAttributeLabel('owner_id')); ?>:</b>
+	<?php echo CHtml::encode($data->owner->username); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('requester_id')); ?>:</b>
+	<?php echo CHtml::encode($data->requester->username); ?>
+	<br />
+
+</div>
